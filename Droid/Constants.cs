342fde634ce7e8b48c645d46ef8dd399d88b7130
LaloSanchez <eduardo_sz5k4@hotmail.com>
2017-11-20 23:00:31 -0600

    se agregao el geofences

---
+++ Droid/Constants.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Android.Gms.Maps.Model;
+
+namespace ecUAQ.Droid
+{
+    public static class Constants
+    {
+        public const string PACKAGE_NAME = "com.andradescompany.ecUAQ";
+        public const string SHARED_PREFERENCES_NAME = PACKAGE_NAME + ".SHARED_PREFERENCES_NAME";
+        public const string GEOFENCES_ADDED_KEY = PACKAGE_NAME + ".GEOFENCES_ADDED_KEY";
+
+        public const long GEOFENCE_EXPIRATION_IN_HOURS = 12;
+        public const long GEOFENCE_EXPIRATION_IN_MILLISECONDS = GEOFENCE_EXPIRATION_IN_HOURS * 60 * 60 * 1000;
+        public const float GEOFENCE_RADIUS_IN_METERS = 1000;
+
+        public static readonly Dictionary<string, LatLng> BAY_AREA_LANDMARKS = new Dictionary<string, LatLng> {
+            { "PJEM", new LatLng (19.2920, -99.647427) },
+            { "World Trade Center", new LatLng (19.393664, -99.1745978) },
+            { "Aeropuerto CDMX", new LatLng (19.4360762, -99.074097) },
+            { "Arizoba", new LatLng (19.3933049, -99.181077) }
+        };
+    }
+}
+
--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,33 +1,234 @@
-﻿using System;
+﻿    using System;
 
-using Android.App;
-using Android.Content;
-using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.OS;
+    using Android.App;
+    using Android.Content;
+    using Android.Content.PM;
+    using Android.Runtime;
+    using Android.Views;
+    using Android.Widget;
+    using Android.OS;
 
-namespace ecUAQ.Droid
-{
-    [Activity(Label = "Cultura UAQ", Icon = "@drawable/logo_proyecto", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    using Android.Support.V7.App;
+    using Android.Gms.Common.Apis;
+    using System.Collections.Generic;
+    using Android.Gms.Location;
+    using Android.Media;
+    using Android.Util;
+    using Java.Lang;
+
+    namespace ecUAQ.Droid
     {
-        protected override void OnCreate(Bundle bundle)
+    [Activity(Label = "Cultura UAQ", Icon = "@drawable/logo_proyecto", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity,
+            GoogleApiClient.IConnectionCallbacks,
+            GoogleApiClient.IOnConnectionFailedListener
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+            protected const string TAG = "BBB";
+            protected GoogleApiClient mGoogleApiClient;
+            protected IList<IGeofence> mGeofenceList;
+            bool mGeofencesAdded;
+            PendingIntent mGeofencePendingIntent;
+            ISharedPreferences mSharedPreferences;
 
-            base.OnCreate(bundle);
+            protected override void OnCreate(Bundle savedInstanceState)
+            {
+                TabLayoutResource = Resource.Layout.Tabbar;
+                ToolbarResource = Resource.Layout.Toolbar;
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+                base.OnCreate(savedInstanceState);
 
-            LoadApplication(new App());
+                mGeofenceList = new List<IGeofence>();
+                mGeofencePendingIntent = null;
+                //RemoveGeofencesButtonHandler();
+                //AddGeofencesButtonHandler();
+                mGeofenceList = new List<IGeofence>();
+                mGeofencePendingIntent = null;
+                Log.Info(TAG, "inicia");
+                mSharedPreferences = GetSharedPreferences(Constants.SHARED_PREFERENCES_NAME,
+                    FileCreationMode.Private);
+                Log.Info(TAG, "mSharedPreferences");
+
+                mGeofencesAdded = mSharedPreferences.GetBoolean(Constants.GEOFENCES_ADDED_KEY, false);
+                Log.Info(TAG, "mGeofencesAdded");
+
+                PopulateGeofenceList();
+                Log.Info(TAG, "PopulateGeofenceList");
+                BuildGoogleApiClient();
+                Log.Info(TAG, "BuildGoogleApiClient");
+                global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+                LoadApplication(new App());
+                Log.Info(TAG, "LoadApplication");
+                //RemoveGeofencesButtonHandler();
+
+            }
+
+            protected void BuildGoogleApiClient()
+            {
+                Log.Info(TAG, "BuildGoogleApiClient");
+                mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .AddConnectionCallbacks(this)
+                    .AddOnConnectionFailedListener(this)
+                    .AddApi(LocationServices.API)
+                    .Build();
+            }
+
+            protected override void OnStart()
+            {
+                Log.Info(TAG, "OnStart");
+                base.OnStart();
+                mGoogleApiClient.Connect();
+
+            }
+
+            protected override void OnStop()
+            {
+                Log.Info(TAG, "OnStop");
+                base.OnStop();
+                mGoogleApiClient.Disconnect();
+            }
+
+            public void OnConnected(Bundle connectionHint)
+            {
+                Log.Info(TAG, "Connected to GoogleApiClient");
+                AddGeofencesButtonHandler();
+                Log.Info(TAG, "AddGeofencesButtonHandler");
+            }
+
+            public void OnConnectionSuspended(int cause)
+            {
+                Log.Info(TAG, "Connection suspended");
+            }
+
+            public void OnConnectionFailed(Android.Gms.Common.ConnectionResult result)
+            {
+                Log.Info(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.ErrorCode);
+            }
+
+            GeofencingRequest GetGeofencingRequest()
+            {
+                Log.Info(TAG, "GetGeofencingRequest");
+                var builder = new GeofencingRequest.Builder();
+                builder.SetInitialTrigger(GeofencingRequest.InitialTriggerEnter);
+                builder.AddGeofences(mGeofenceList);
+
+                return builder.Build();
+            }
+
+            public async void AddGeofencesButtonHandler()
+            {
+                Log.Info(TAG, "AddGeofencesButtonHandler");
+                if (!mGoogleApiClient.IsConnected)
+                {
+                    Log.Info(TAG, "mGoogleApiClient.IsConnected");
+                    Toast.MakeText(this, GetString(Resource.String.not_connected), ToastLength.Short).Show();
+                    return;
+                }
+
+                try
+                {
+                    var status = await LocationServices.GeofencingApi.AddGeofencesAsync(mGoogleApiClient, GetGeofencingRequest(),
+                        GetGeofencePendingIntent());
+                    HandleResult(status);
+                }
+                catch (SecurityException securityException)
+                {
+                    Log.Info(TAG, "SecurityException");
+                    LogSecurityException(securityException);
+                }
+            }
+
+            public async void RemoveGeofencesButtonHandler()
+            {
+                Log.Info(TAG, "RemoveGeofencesButtonHandler");
+                if (!mGoogleApiClient.IsConnected)
+                {
+                    Log.Info(TAG, "mGoogleApiClient.IsConnected");
+                    Toast.MakeText(this, GetString(Resource.String.not_connected), ToastLength.Short).Show();
+                    return;
+                }
+                try
+                {
+                    var status = await LocationServices.GeofencingApi.RemoveGeofencesAsync(mGoogleApiClient,
+                        GetGeofencePendingIntent());
+                    HandleResult(status);
+                }
+                catch (SecurityException securityException)
+                {
+                    Log.Info(TAG, "SecurityException");
+                    LogSecurityException(securityException);
+                }
+            }
+
+            void LogSecurityException(SecurityException securityException)
+            {
+                Log.Error(TAG, "Invalid location permission. " +
+                    "You need to use ACCESS_FINE_LOCATION with geofences", securityException);
+            }
+
+            public void HandleResult(Statuses status)
+            {
+                Log.Info(TAG, "HandleResult");
+                if (status.IsSuccess)
+                {
+                    Log.Info(TAG, "status.IsSuccess");
+                    mGeofencesAdded = !mGeofencesAdded;
+                    var editor = mSharedPreferences.Edit();
+                    editor.PutBoolean(Constants.GEOFENCES_ADDED_KEY, mGeofencesAdded);
+                    editor.Commit();
+
+
+                    Toast.MakeText(
+                        this,
+                        GetString(mGeofencesAdded ? Resource.String.geofences_added :
+                            Resource.String.geofences_removed),
+                        ToastLength.Short
+                    ).Show();
+                }
+                else
+                {
+                    var errorMessage = GeofenceErrorMessages.GetErrorString(this,
+                        status.StatusCode);
+                    Log.Error(TAG, errorMessage);
+                }
+            }
+
+            PendingIntent GetGeofencePendingIntent()
+            {
+                Log.Info(TAG, "GetGeofencePendingIntent");
+                if (mGeofencePendingIntent != null)
+                {
+                    return mGeofencePendingIntent;
+                }
+                var intent = new Intent(this, typeof(GeofenceTransitionsIntentService));
+                return PendingIntent.GetService(this, 0, intent, PendingIntentFlags.UpdateCurrent);
+            }
+
+            public void PopulateGeofenceList()
+            {
+                Log.Info(TAG, "PopulateGeofenceList");
+                foreach (var entry in Constants.BAY_AREA_LANDMARKS)
+                {
+                    mGeofenceList.Add(new GeofenceBuilder()
+                        .SetRequestId(entry.Key)
+                        .SetCircularRegion(
+                            entry.Value.Latitude,
+                            entry.Value.Longitude,
+                            Constants.GEOFENCE_RADIUS_IN_METERS
+                        )
+                        .SetExpirationDuration(Constants.GEOFENCE_EXPIRATION_IN_MILLISECONDS)
+                        .SetTransitionTypes(Geofence.GeofenceTransitionEnter |
+                            Geofence.GeofenceTransitionExit)
+                        .Build());
+                }
+            }
+
+            protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+            {
+                base.OnActivityResult(requestCode, resultCode, data);
+            }
+
         }
 
-        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
-        {
-            base.OnActivityResult(requestCode, resultCode, data);
-        }
     }
-}
