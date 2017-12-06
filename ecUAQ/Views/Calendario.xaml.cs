d77fc96e8e5dda4f1c7dad90a6ade6505395378f
antonioantunez7 <scorpion_malboro@hotmail.com>
2017-12-05 21:52:36 -0600

    a

--- ecUAQ/Views/Calendario.xaml.cs
+++ ecUAQ/Views/Calendario.xaml.cs
@@ -27,10 +27,9 @@ namespace ecUAQ.Views
                     SpecialDates.Add(
                         new SpecialDate(DateTime.Parse((ev.fechaInicio)))
                         {
-                            BackgroundImage = "calendario.png",
+                            //BackgroundImage = "calendario.png",
 
-                            //BackgroundColor = Color.Turquoise,
-                            //TextColor = Color.White,
+                        BackgroundColor = Color.DarkBlue,                         //TextColor = Color.White,
                             TextColor = Color.Black,
                             //BorderColor = Color.Aqua,
                             //BorderWidth = 5,
