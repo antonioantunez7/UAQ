555db36bf355115e2b26256eb8afd9dd2f016c5c
antonioantunez7 <scorpion_malboro@hotmail.com>
2018-01-10 16:54:29 -0600

    m

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,4 +1,4 @@
-﻿    using System;
+﻿    /*using System;
 
     using Android.App;
     using Android.Content;
@@ -289,3 +289,39 @@ namespace ecUAQ.Droid
         }
 
     }
+
+*/
+
+using System;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace ecUAQ.Droid
+{
+     [Activity(Label = "Cultura UAQ", Icon = "@drawable/logo_proyecto", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+     //[Activity(Label = "ecUAQ.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+     {
+         protected override void OnCreate(Bundle bundle)
+         {
+             TabLayoutResource = Resource.Layout.Tabbar;
+             ToolbarResource = Resource.Layout.Toolbar;
+ 
+             base.OnCreate(bundle);
+ 
+             global::Xamarin.Forms.Forms.Init(this, bundle);
+            XamForms.Controls.Droid.Calendar.Init();  // <<<<<!!! Please insert this
+             LoadApplication(new App());
+         }
+ 
+         protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+         {
+             base.OnActivityResult(requestCode, resultCode, data);
+         }
+     }
+ }
