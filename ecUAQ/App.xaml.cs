b8111aff0a0cdf06fdc80052d0a24e640a38512e
antonioantunez7 <scorpion_malboro@hotmail.com>
2018-02-17 18:15:59 -0600

    mm

--- ecUAQ/App.xaml.cs
+++ ecUAQ/App.xaml.cs
@@ -4,6 +4,8 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using ecUAQ.Models;
 using ecUAQ.Views;
+using Plugin.Geofencing;
+using Plugin.Notifications;
 using Xamarin.Forms;
 using XamForms.Controls;
 
@@ -39,6 +41,26 @@ namespace ecUAQ
             //calendar.SpecialDates.Add(new SpecialDate(DateTime.Now.AddDays(6)) { BackgroundColor = Color.Fuchsia, TextColor = Color.Accent, BorderColor = Color.Maroon, BorderWidth = 8 });
             //calendar.RaiseSpecialDatesChanged();
 
+            // Handle when your app starts
+            CrossGeofences.Current.StartMonitoring(new GeofenceRegion(
+            "My House", // identifier - must be unique per registered geofence
+                //99.675636
+                new Position(19.273603, -99.675636), // center point    
+            Distance.FromKilometers(1) // radius of fence
+            ));
+
+            base.OnStart();
+            CrossGeofences.Current.RegionStatusChanged += (sender, args) =>
+            {
+
+
+                var msg = $"Geofence status for {args.Region.Identifier} changed to {args.Status}";
+                CrossNotifications.Current.Send(new Notification
+                {
+                    Title = "Geofence Update",
+                    Message = msg
+                });
+            };
         }
     }
 }
